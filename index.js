b4611a6d6af5b09ce646d423281b4ac24f209de1
yania15 <yn141@scarletmail.rutgers.edu>
2025-12-18 16:59:31 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,41 +1,15 @@
 function changeImage() {
   var img = document.getElementById('myImage');
-  if (img.src.match("image1.jpg")) {
-    img.src = "sitches2.png";
-  } else {
+  const label = document.getElementById("stitchesLabel");
+
+  if (img && img.src.includes("sitches2.png")) {
     img.src = "jeanhole.png";
+  } else if (img) {
+    img.src = "sitches2.png";
   }
+
+  if (label) label.classList.remove("hidden");
 
 }
 const helvetica4 = document.getElementById("helvetica4");
 const xray = document.getElementById("xray");
-
-document.addEventListener("mousemove", (e) => {
-  const rect = xray.getBoundingClientRect();
-
-  const x = e.clientX - rect.left;
-  const y = e.clientY - rect.top;
-
-
-  const r = 100; // 
-
-  // update mask
-  helvetica4.style.clipPath = `circle(${r}px at ${x}px ${y}px)`;
-});
-
-const mars = document.getElementById("mars");
-const container = document.getElementById("container");
-
-document.addEventListener("mousemove", (e) => {
-  const rect = container.getBoundingClientRect();
-
-  // cursor position relative to image
-  const x = e.clientX - rect.left;
-  const y = e.clientY - rect.top;
-
-  // size of the reveal circle
-  const r = 100; // <-- change radius here
-
-  // update mask
-  mars.style.clipPath = `circle(${r}px at ${x}px ${y}px)`;
-});
